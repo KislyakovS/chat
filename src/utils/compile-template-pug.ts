d770df0a8199689e5c429c8e5d4099825ac75991
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-10-01 13:32:07 +0300

    feat: compile `+block`

--- src/utils/compile-template-pug.ts
+++ src/utils/compile-template-pug.ts
@@ -1,12 +1,25 @@
 import { compile } from 'pug';
-import type { DefaultProps } from '../core/component';
+import Component, { DefaultProps } from '../core/component';
 
 export default <T extends Pick<DefaultProps, 'children'>>(template: string, props: T) => {
 	const fragment = document.createElement('template');
 
+	const childrenInComponent: Component<DefaultProps>[] = [];
+	const childrenInBlock: Component<DefaultProps>[] = [];
+
 	props.children?.forEach((child) => {
+		if (template.includes(`+${child.constructor.name}`)) {
+			childrenInComponent.push(child);
+		} else {
+			childrenInBlock.push(child);
+		}
+	});
+
+	childrenInComponent.forEach((child) => {
 		template = template.replace(`+${child.constructor.name}`, `<div data-id="${child.id}"></div>`);
 	});
+
+	template = template.replace('+block', childrenInBlock.map((child) => `<div data-id="${child.id}"></div>`).join(''));
 
 	fragment.innerHTML = compile(template)(props);
 
