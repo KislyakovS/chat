8cdcc1c996f88590ae4e65710d4cf37117f6bf81
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-10-01 14:06:31 +0300

    feat: add module `sign-up`

---
+++ src/modules/forms/sign-up/sign-up.ts
@@ -0,0 +1,70 @@
+import Component, { DefaultProps } from '../../../core/component';
+
+import Form from '../../../components/form';
+import Button from '../../../components/button';
+import Field from '../../../components/field';
+
+import template from './sign-up.tmpl';
+
+type Props = DefaultProps;
+
+export default class SignUp extends Component<Props> {
+	constructor(props: Props = {}) {
+		const email = new Field({
+			error: 'Invalid mail',
+			type: 'email',
+			placeholder: 'Email',
+			name: 'email',
+		});
+		const login = new Field({
+			error: 'Invalid login',
+			placeholder: 'Login',
+			name: 'login',
+		});
+		const name = new Field({
+			error: 'Invalid name',
+			placeholder: 'Name',
+			name: 'first_name',
+		});
+		const lastName = new Field({
+			error: 'Invalid second name',
+			placeholder: 'Last Name',
+			name: 'second_name',
+		});
+		const phone = new Field({
+			error: 'Invalid phone',
+			type: 'tel',
+			placeholder: 'Phone',
+			name: 'phone',
+		});
+		const password = new Field({
+			error: 'Invalid password',
+			type: 'password',
+			placeholder: 'Password',
+			name: 'password',
+		});
+		const button = new Button({
+			type: 'submit',
+			text: 'Sign up',
+		});
+
+		const form = new Form({
+			children: [email, login, name, lastName, phone, password, button],
+			events: {
+				submit: (e) => {
+					e.preventDefault();
+					console.log({
+						email: email.value,
+						login: login.value,
+						name: name.value,
+						lastName: lastName.value,
+						phone: phone.value,
+						password: password.value,
+					});
+				},
+			},
+		});
+
+		super(template, Component.setChildrenInProps(props, form));
+	}
+}
