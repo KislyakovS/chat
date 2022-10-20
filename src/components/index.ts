b685109ca9ea119e38b254ec58c40bed5b0d6216
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-10-20 20:32:54 +0300

    feat: add component `ErrorMessage`

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Back } from './back';
 export { default as Box } from './box';
 export { default as Button } from './button';
 export { default as ChangeAvatar } from './change-avatar';
+export { default as ErrorMessage } from './error-message';
 export { default as ErrorMessagePage } from './error-message-page';
 export { default as Field } from './field';
 export { default as Form } from './form';
