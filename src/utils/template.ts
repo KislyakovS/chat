e14d5650fd6db0032f4f3fe141d32d3010229fe2
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-10-25 12:16:38 +0300

    feat: `Template`

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -1,9 +1,9 @@
-import type { Children } from '../core/component';
+import type { Children, Events } from '../core/component';
 
 class Template {
 	private readonly _parse = new DOMParser();
 
-	public compile(template: string, children: Children = {}) {
+	public compile(template: string, children: Children = {}, events: Events = []) {
 		const fragment = document.createDocumentFragment();
 		const { firstElementChild: firstChild } = this._toXml(template);
 
@@ -23,44 +23,44 @@ class Template {
 				props.children = firstChild.innerHTML;
 			}
 
-			const component = new children[localName](props, { children });
+			const component = new children[localName](props, { children, events });
 			fragment.append(component.element);
 		} else if (firstChild.localName === 'svg') {
 				fragment.append(this._toHtml(firstChild.outerHTML)[0]);
-			} else {
-				const parentElement = this._toHtml(firstChild.outerHTML)[0].cloneNode();
+		} else {
+			const parentElement = this._toHtml(firstChild.outerHTML)[0].cloneNode();
 
-				firstChild.childNodes.forEach((child) => {
-					if (!this._isElement(child)) {
-						if (child.textContent?.trim() !== '') {
-							parentElement.textContent = child.textContent;
-						}
-						return;
+			firstChild.childNodes.forEach((child) => {
+				if (!this._isElement(child)) {
+					if (child.textContent?.trim() !== '') {
+						parentElement.textContent = child.textContent;
+					}
+					return;
+				}
+
+				const { localName, childNodes } = child;
+
+				if (this._isCustomElement(child)) {
+					if (!(localName in children)) {
+						throw new Error(`There is no component named '${localName}'.`);
 					}
 
-					const { localName, childNodes } = child;
-
-					if (this._isCustomElement(child)) {
-						if (!(localName in children)) {
-							throw new Error(`There is no component named '${localName}'.`);
-						}
-
-						const props: Record<string, unknown> = this._mapAttributes(child.attributes);
-						if (childNodes.length) {
-							props.children = child.innerHTML;
-						}
-
-						const component = new children[localName](props, { children });
-						parentElement.appendChild(component.element);
-					} else {
-						parentElement.appendChild(this.compile(child.outerHTML, children));
+					const props: Record<string, unknown> = this._mapAttributes(child.attributes);
+					if (childNodes.length) {
+						props.children = child.innerHTML;
 					}
-				});
 
-				fragment.append(parentElement);
-			}
+					const component = new children[localName](props, { children, events });
+					parentElement.appendChild(component.element);
+				} else {
+					parentElement.appendChild(this.compile(child.outerHTML, children, events));
+				}
+			});
 
-		return fragment as unknown as HTMLElement;
+			fragment.append(parentElement);
+		}
+
+		return fragment.firstElementChild as HTMLElement;
 	}
 
 	private _toXml(str: string) {
