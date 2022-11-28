d197055d620c0f79b0c70f7aa0002ab5d0a6034c
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-11-28 19:30:12 +0300

    feat: conroller logout

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,4 @@ export { default as signUpController } from './sign-up';
 export { default as profileChangeController } from './profile-change';
 export { default as passwordChangeController } from './password-change';
 export { default as profileController } from './profile';
+export { default as logoutController } from './logout';
