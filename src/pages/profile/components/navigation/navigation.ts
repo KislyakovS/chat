95b25fd356d6183573f6a649b20099bedf0c3202
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-11-28 19:31:01 +0300

    feat: exit the chat

--- src/pages/profile/components/navigation/navigation.ts
+++ src/pages/profile/components/navigation/navigation.ts
@@ -1,10 +1,22 @@
-import Component from '../../../../core/component';
+import Component, { Events } from '../../../../core/component';
 
-import { Link } from '../../../../components';
+import { Link, Button } from '../../../../components';
+
+import { logoutController } from '../../../../controllers';
 
 export default class Navigation extends Component {
+	private async _onClickButtonExit(e: Event) {
+		e.preventDefault();
+
+		await logoutController.logout();
+	}
+
 	protected children() {
-		return { Link };
+		return { Link, Button };
+	}
+
+	protected events(): Events {
+		return [{ name: 'click', listener: this._onClickButtonExit.bind(this) }];
 	}
 
 	render() {
@@ -13,7 +25,7 @@ export default class Navigation extends Component {
 			<ul class="list">
 				<li><Link href="/settings">Change Data</Link></li>
 				<li><Link href="/password">Change Password</Link></li>
-				<li><Link isAccent="true">Exit</Link></li>
+				<li><Button style="link-accent" onClick="${this._onClickButtonExit.name}">Exit</Button></li>
 			</ul>
 		</nav>
 		`;
