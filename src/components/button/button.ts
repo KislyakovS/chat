7e0790b4ac1c4393c7771f75775365db5b98be2b
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-11-28 19:29:05 +0300

    feat: add button props `style`

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -4,20 +4,26 @@ import clsx from '../../utils/clsx';
 
 import type { ButtonType } from '../../types';
 
+type Style = 'default' | 'link' | 'link-accent';
+
 type Props = {
 	className?: string,
 	type?: ButtonType,
 	children: string,
+	style?: Style,
+	onClick?: string,
 }
 
 export default class Button extends Component<Props> {
 	render() {
-		const { className, type = 'button', children } = this.props;
+		const {
+			className, onClick, type = 'button', style = 'default', children,
+		} = this.props;
 
-		const cls = clsx('button', className);
+		const cls = clsx('button', className, style !== 'default' && `button_${style}`);
 
 		return `
-		<button class="${cls}" type="${type}">
+		<button class="${cls}" type="${type}" click="${onClick}">
 			${children}
 		</button>
 		`;
