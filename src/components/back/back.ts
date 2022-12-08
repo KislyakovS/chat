8ded77c23d096e2b6dcef83f0269ebc7418b8299
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-12-08 13:28:53 +0300

    feat: test

--- src/components/back/back.ts
+++ src/components/back/back.ts
@@ -4,25 +4,25 @@ import router from '../../core/router';
 import IconButton from '../icon-button';
 
 export default class Back extends Component {
-	private _onClickBack(e: Event) {
-		e.preventDefault();
+  private _onClickBack(e: Event) {
+    e.preventDefault();
 
-		router.back();
-	}
+    router.back();
+  }
 
-	protected children() {
-		return { IconButton };
-	}
+  protected children() {
+    return { IconButton };
+  }
 
-	protected events(): Events {
-		return [{ name: 'click', listener: this._onClickBack }];
-	}
+  protected events(): Events {
+    return [{ name: 'click', listener: this._onClickBack }];
+  }
 
-	render() {
-		return `
+  render() {
+    return `
 		<div class="back">
 			<IconButton onClick="${this._onClickBack.name}" icon="arrow-left" />
 		</div>
 		`;
-	}
+  }
 }
