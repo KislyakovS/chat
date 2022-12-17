1fb60a8f05ac5d2e54209d547c445e92acf40e51
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-12-17 20:54:23 +0300

    fix: error no root element

--- src/core/router.ts
+++ src/core/router.ts
@@ -3,80 +3,80 @@ import Page from './page';
 import type { Path, Constructor } from '../types';
 
 class Router {
-	private _routes: Record<string, Constructor<Page>> = {};
-	private _rootElement?: HTMLElement;
-	private readonly _history: History;
-	private _NotFound?: Constructor<Page>;
+  private _routes: Record<string, Constructor<Page>> = {};
+  private _rootElement?: Element;
+  private readonly _history: History;
+  private _NotFound?: Constructor<Page>;
 
-	constructor() {
-		this._history = window.history;
-	}
+  constructor() {
+    this._history = window.history;
+  }
 
-	public start(root: HTMLElement) {
-		this._rootElement = root;
+  public start(root: Element) {
+    this._rootElement = root;
 
-		window.addEventListener('popstate', this._render.bind(this));
+    window.addEventListener('popstate', this._render.bind(this));
 
-		this._render();
-	}
+    this._render();
+  }
 
-	private _render() {
-		if (!this._rootElement) {
-			throw new Error('There is no root element');
-		}
+  private _render() {
+    if (!this._rootElement) {
+      throw new Error('There is no root element');
+    }
 
-		let page: Page;
+    let page: Page;
 
-		if (this._isPathRouters(this._currentPath)) {
-			page = new this._routes[this._currentPath]();
-		} else {
-			if (!this._NotFound) {
-				throw new Error('Not page `not found`. Set by method `notFound`');
-			}
+    if (this._isPathRouters(this._currentPath)) {
+      page = new this._routes[this._currentPath]();
+    } else {
+      if (!this._NotFound) {
+        throw new Error('Not page `not found`. Set by method `notFound`');
+      }
 
-			page = new this._NotFound();
-		}
+      page = new this._NotFound();
+    }
 
-		this._rootElement.innerHTML = '';
-		this._rootElement.append(page.element);
-	}
+    this._rootElement.innerHTML = '';
+    this._rootElement.append(page.element);
+  }
 
-	private _isPathRouters(path: Path) {
-		return path in this._routes;
-	}
+  private _isPathRouters(path: Path) {
+    return path in this._routes;
+  }
 
-	private get _currentPath(): Path {
-		return document.location.pathname as Path;
-	}
+  private get _currentPath(): Path {
+    return document.location.pathname as Path;
+  }
 
-	public use(path: Path, page: Constructor<Page>) {
-		this._routes[path] = page;
+  public use(path: Path, page: Constructor<Page>) {
+    this._routes[path] = page;
 
-		return this;
-	}
+    return this;
+  }
 
-	public notFound(page: Constructor<Page>) {
-		this._NotFound = page;
+  public notFound(page: Constructor<Page>) {
+    this._NotFound = page;
 
-		return this;
-	}
+    return this;
+  }
 
-	public go(path: Path) {
-		if (path in this._routes) {
-			this._history.pushState({}, '', path);
-			this._render();
-		} else {
-			throw new Error(`Pages with this path '${path}' is missing. Add via router.use(path, component)`);
-		}
-	}
+  public go(path: Path) {
+    if (path in this._routes) {
+      this._history.pushState({}, '', path);
+      this._render();
+    } else {
+      throw new Error(`Pages with this path '${path}' is missing. Add via router.use(path, component)`);
+    }
+  }
 
-	public back() {
-		this._history.back();
-	}
+  public back() {
+    this._history.back();
+  }
 
-	public forward() {
-		this._history.forward();
-	}
+  public forward() {
+    this._history.forward();
+  }
 }
 
 export default new Router();
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,23 +1,27 @@
 import {
- SignUp, Login, Messager, ProfileChange, PasswordChange, Profile, NotFound,
+  SignUp, Login, Messager, ProfileChange, PasswordChange, Profile, NotFound,
 } from '../pages';
 import router from '../core/router';
 import http from '../core/http';
 
 import { Routes } from '../constants';
 
-const app = document.querySelector('#app') as HTMLElement;
+const app = document.querySelector('#app');
+
+if (!app) {
+  throw new Error('No root element by id `app`')
+}
 
 if (process.env.CLIENT_API_URL) {
-	http.setup({ baseURL: process.env.CLIENT_API_URL });
+  http.setup({ baseURL: process.env.CLIENT_API_URL });
 }
 
 router
-	.use(Routes.login, Login)
-	.use(Routes.signUp, SignUp)
-	.use(Routes.messenger, Messager)
-	.use(Routes.profile, Profile)
-	.use(Routes.settings, ProfileChange)
-	.use(Routes.password, PasswordChange)
-	.notFound(NotFound)
-	.start(app);
+  .use(Routes.login, Login)
+  .use(Routes.signUp, SignUp)
+  .use(Routes.messenger, Messager)
+  .use(Routes.profile, Profile)
+  .use(Routes.settings, ProfileChange)
+  .use(Routes.password, PasswordChange)
+  .notFound(NotFound)
+  .start(app);
